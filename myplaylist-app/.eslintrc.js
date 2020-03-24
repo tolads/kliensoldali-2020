33cffdf97565da9e7295ab0cf245ea1c60379fed
Tamás László <tlaszlods@gmail.com>
2020-03-24 13:04:17 +0100

    myplaylist-app előkészítés

---
+++ myplaylist-app/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module"
+  },
+  extends: ["react-app", "plugin:prettier/recommended"],
+  env: {
+    jest: true
+  },
+  globals: {
+    window: true,
+    document: true
+  }
+};
